551b59e56b1b77500f58f76b66794da1593a870d
ekrzeptowski <kontakt@ekrzeptowski.pl>
2023-11-22 12:54:34 +0100

    Lab3 example

---
+++ lab3-app/Controllers/ContactController.cs
@@ -0,0 +1,115 @@
+﻿using lab3_app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3_app.Controllers
+{
+    public class ContactController : Controller
+    {
+        static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>()
+        {
+            {1, new Contact(){ Id=1, Name="Stefan", Email="a@asas.com", Phone="123456789", Birth=new DateTime(1980,1,3)} },
+            {2, new Contact(){ Id=2, Name="Jan", Email="j@asas.com", Phone="1122334455", Birth=new DateTime(1990,3,12)}},
+            {3, new Contact(){ Id=3, Name="Anna", Email="a@asas.com", Phone="678543445", Birth=new DateTime(1985,4,21)}}
+        };
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute] int id)
+        {
+            if (_contacts.ContainsKey(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_contacts.ContainsKey(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete([FromForm] Contact contact)
+        {
+            if (_contacts.ContainsKey(contact.Id))
+            {
+                _contacts.Remove(contact.Id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_contacts.ContainsKey(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _contacts.Keys.Count != 0 ? _contacts.Keys.Max() : 0;
+                contact.Id = id + 1;
+                _contacts.Add(contact.Id, contact);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromForm] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_contacts.ContainsKey(contact.Id))
+                {
+                    _contacts[contact.Id] = contact;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ lab3-app/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3_app.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Podaj imię")]
+        [StringLength(50, ErrorMessage = "Imię może zawierać maksymalnie 50 znaków")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Podaj adres e-mail")]
+        [EmailAddress(ErrorMessage = "Podaj poprawny adres e-mail")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Podaj poprawny numer telefonu")]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Podaj poprawną datę")]
+        public DateTime Birth { get; set; }
+    }
+}
